af93dae5527d71ab2dafc9f75c7797b4a4d7ddf0
Яхия <104417808+Yaxi4@users.noreply.github.com>
2022-06-09 11:55:17 +0500

    Add files via upload

---
+++ __call__.py
@@ -0,0 +1,25 @@
+class Counter:
+    def __init__(self):
+        self.count = 0
+        self.summa = 0
+        self.lengh = 0
+        self.srednee = 0
+
+    def __call__(self, *args, **kwargs):
+        self.count += 1
+        self.summa += sum(args)
+        self.lengh += len(args)
+        self.srednee += self.summa / self.lengh
+        print(f'наш экземпляр вызывался {self.count} раз')
+        print(self.summa, 'ghbdt')
+
+b=Counter()
+b(100,200,300)
+print('-----------------')
+
+a=Counter()
+a(4,5)
+print(a.summa)
+print(a.lengh)
+print(a.srednee)
+print('-----------------')
